3ea4b9fb43c7cd77bf672b9d11b91fe23b78f4db
Cesar Pimentel <cpimentelortiz23@gmail.com>
2025-03-13 13:14:43 -0400

    Cinemachine added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,18 +6,53 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
 
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+
+    [SerializeField] float thrustStrenght = 10f;
+    [SerializeField] float rotationStrength = 10f;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     // Input action enabled, it can also be disabled and called multiple times
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
-    private void Update()
+    private void FixedUpdate()
+    {
+        ProccessThrust();
+        ProcessRotation();
+    }
+
+    private void ProccessThrust()
     {
         if (thrust.IsPressed())
         {
-            Debug.Log("The Thrust is Thrusting!!");
+            rb.AddRelativeForce(Vector3.up * thrustStrenght * Time.fixedDeltaTime);
         }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+    }
+
+    private void ApplyRotation(float rotationPerFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationPerFrame * Time.fixedDeltaTime);
     }
 }
